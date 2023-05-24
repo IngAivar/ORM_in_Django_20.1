c1d53ea858ea4580ce0f7c3f9bf36179bf5f7350
MiniGan067 <MiniGan067@yandex.ru>
2023-05-24 17:02:26 +0300

    add admin showcase

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from catalog.models import Product, Category
+
+# admin.site.register(Product)
+# admin.site.register(Category)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'category')
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'product_name', 'purchase_price', 'category')
