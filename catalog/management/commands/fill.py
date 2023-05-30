dcb47652f20218e611d8d598dca702420dca9a72
MiniGan067 <MiniGan067@yandex.ru>
2023-05-30 18:04:20 +0300

    fix done

--- catalog/management/commands/fill.py
+++ catalog/management/commands/fill.py
@@ -6,6 +6,22 @@ from catalog.models import Product, Category
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
+        category_list = [
+            {'id': 1, 'category': 'Бытовая техника',
+             'description': 'это техника, используемая в быту, предназначенная для'
+                            ' облегчения ручного и монотонного труда, а'
+                            ' так же повышению комфорта. Она может быть'
+                            ' как электрической, так и механической.'},
+            {'id': 2, 'category': 'Встраиваемая бытовая техника', 'description': 'это техника, используемая в быту,'},
+            {'id': 3, 'category': 'ТВ и приставки', 'description': 'Небольшое устройство для приема волн с последующей'
+                                                                   ' передачей информации на телевизоры любого типа. '},
+            {'id': 4, 'category': 'Климатическое оборудование',
+             'description': 'Оборудование, основанное на работе холодильных'
+                            ' машин, предназначенное для автоматического'
+                            ' поддержания температуры и иных параметров'
+                            ' воздуха в закрытых помещениях или'
+                            ' термоизолированных камерах.'},
+        ]
         product_list = [
             {'product_name': 'Игровая приставка Microsoft Xbox Series X 1TB',
              'description': 'Приставка',
@@ -42,6 +58,14 @@ class Command(BaseCommand):
              'category': 2}
         ]
 
+        category_for_create = []
+        for category_item in category_list:
+            category_for_create.append(
+                Category(**category_item)
+            )
+
+        Category.objects.bulk_create(category_for_create)
+
         product_for_create = []
         for product_item in product_list:
             product_for_create.append(
@@ -56,18 +80,3 @@ class Command(BaseCommand):
             )
 
         Product.objects.bulk_create(product_for_create)
-
-
-'''
-for data in json_data:
-                if data['model'] == 'catalog.product':
-                    # Category.objects.get(pk=1)
-                    product_for_create.append(Product(
-                        name=data['fields']['name'],
-                        description=data['fields']['description'],
-                        category=Category.objects.get(pk=data['fields']['category']),
-                        price=data['fields']['price'],
-                        create_date=data['fields']['create_date'],
-                        last_change_date=data['fields']['last_change_date'],
-                    ))
-'''
