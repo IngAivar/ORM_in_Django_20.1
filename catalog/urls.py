be156bcdc785476e1c16b116e8f996abe5f6dfa0
MiniGan067 <MiniGan067@yandex.ru>
2023-06-02 17:47:00 +0300

    21.1 fbv and cbv first step

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,11 +2,16 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import home, contacts
+from catalog.views import ProductListView, contacts
+
+# from catalog.views import home, contacts
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', home, name='home'),
+    path('', ProductListView.as_view(), name='product_list'),
     path('contacts/', contacts, name='contacts'),
+
+    # path('', home, name='home'),
+    # path('contacts/', contacts, name='contacts'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,16 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
 from catalog.models import Product
 
 
-def home(request):
-    product_list = Product.objects.all()
-    context = {
-        'object_list': product_list,
-        'title': 'Главная'
-    }
-
-    return render(request, 'catalog/home.html', context)
+class ProductListView(ListView):
+    model = Product
 
 
 def contacts(request):
@@ -20,3 +15,15 @@ def contacts(request):
 
     return render(request, 'catalog/contacts.html', context)
 
+# from catalog.models import Product
+#
+#
+# def home(request):
+#     product_list = Product.objects.all()
+#     context = {
+#         'object_list': product_list,
+#         'title': 'Главная'
+#     }
+#
+#     return render(request, 'catalog/product_list.html', context)
+#
