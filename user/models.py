e454eff0549d3bcd9807863c568efc33dcac33ef
MiniGan067 <MiniGan067@yandex.ru>
2023-06-20 13:02:41 +0300

    hw 22.2 update user app (add managers.py)

--- user/models.py
+++ user/models.py
@@ -1,3 +1,32 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from user.managers import CustomUserManager
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class CustomUser(AbstractUser):
+    """
+    Модель, описывающая пользователя сервиса рассылок.
+    Наследуется от AbstractUser.
+    """
+    objects = CustomUserManager()
+
+    username = None
+    email = models.EmailField(unique=True, verbose_name='Электронная почта')
+    email_verified = models.BooleanField(default=False)
+    avatar = models.ImageField(upload_to='users/', verbose_name='Аватар', default='users/default.png')
+    phone = models.CharField(max_length=15, verbose_name='Телефон')
+    country = models.CharField(max_length=100, verbose_name='Страна')
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+        db_table = 'users'
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
