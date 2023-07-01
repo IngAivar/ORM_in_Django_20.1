3138e9d4b2c4baa9436adf74ef9188c26e961588
MiniGan067 <MiniGan067@yandex.ru>
2023-07-02 01:31:15 +0300

    hw 23.2 task (1)

--- config/settings.py
+++ config/settings.py
@@ -134,3 +134,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+CACHE_ENABLED = True
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379",
+        "TIMEOUT": 60
+    }
+}
